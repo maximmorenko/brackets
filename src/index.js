5a8d5547de33b61abc4c94235fe79780556c0180
Maksym <morenko.maxim@gmail.com>
2023-08-11 11:00:14 +0300

    config123457

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config1 = [['(', ')']];
+  const config2 = [['(', ')'], ['[', ']']];
+  const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+  const config4 = [['|', '|']];
+  const config5 = [['(', ')'], ['|', '|']];
+  const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+  const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+
+  if(bracketsConfig!==config6) {
+    function check1(str) {
+      let regex = /\(\)|\[\]|\{\}|\|\|/;
+      return regex.test(str) ? check1(str.replace(regex, '')) : '' === str;
+    }
+    return check1(str);
+  }
+  return true;
 }
